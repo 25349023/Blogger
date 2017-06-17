a0fe0a88c0f98ea472102612c722ffb036abeacb
25349023 <25349023.qq@gmail.com>
2017-06-17 23:39:50 +0800

    Update dfs1.cpp

--- DFS/dfs1.cpp
+++ DFS/dfs1.cpp
@@ -12,14 +12,14 @@ public:
 };
 
 Node* buildTree(){
-    Node* root = new Node("red 1");
-    root->next[0] = new Node("orange 2");
-    root->next[1] = new Node("lime 3");
-    root->next[2] = new Node("green 4");
-    root->next[0]->next[0] = new Node("yellow 5");
-    root->next[2]->next[0] = new Node("blue 6");
-    root->next[2]->next[1] = new Node("violet 7");
-
+    Node* root = new Node("red-1");
+    root->next[0] = new Node("orange-2");
+    root->next[1] = new Node("lime-3");
+    root->next[2] = new Node("green-4");
+    root->next[0]->next[0] = new Node("yellow-5");
+    root->next[2]->next[0] = new Node("blue-6");
+    root->next[2]->next[1] = new Node("violet-7");
+    
     return root;
 }
 
@@ -27,7 +27,7 @@ void dfs(Node* start){
     if (start == nullptr){
         return;
     }
-    cout << start->data << " visited." << endl;
+    cout << start->data << "  visited." << endl;
     for (int i = 0; i < 3; i++){
         dfs(start->next[i]);
     }
