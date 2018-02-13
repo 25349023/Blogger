72306065d9e5c60950481a33486e11964887bfe0
25349023 <25349023.qq@gmail.com>
2018-02-13 15:01:09 +0800

    Create ranking.py

---
+++ DFS/ranking.py
@@ -0,0 +1,20 @@
+visited = [False] * 5
+arrangement = [''] * 3
+people = ['甲', '乙', '丙', '丁', '戊']
+
+
+def dfs(layer):
+    if layer == 3:
+        print(*arrangement, sep='  ')
+        return
+
+    for i in range(5):
+        if visited[i]:
+            continue
+        visited[i] = True
+        arrangement[layer] = people[i]
+        dfs(layer+1)
+        visited[i] = False
+
+
+dfs(0)
