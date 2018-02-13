811a84d3013b0de496f5c31ee4b262cae38bb30c
25349023 <25349023.qq@gmail.com>
2018-02-13 14:05:54 +0800

    Update candyDistribution.py
    add comments

--- DFS/candyDistribution.py
+++ DFS/candyDistribution.py
@@ -1,4 +1,4 @@
-current_count = [2, 1, 1]
+current_count = [2, 1, 1]  # 用來儲存各還有幾顆糖果
 arrangement = [''] * 4
 candy = {0: '紅', 1: '藍', 2: '黃'}
 
@@ -9,12 +9,12 @@ def dfs(layer):
         return
 
     for i in range(3):
-        if current_count[i] == 0:
+        if current_count[i] == 0:  # 如果沒有剩下了
             continue
-        current_count[i] -= 1
+        current_count[i] -= 1  # 用掉一顆
         arrangement[layer] = candy[i]
-        dfs(layer+1)
+        dfs(layer + 1)  # 下一層 DFS
         current_count[i] += 1
-
+        
 
 dfs(0)
